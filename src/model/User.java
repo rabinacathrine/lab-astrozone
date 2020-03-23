28af24428d050dbb7ba737ba88bdad0ad5a78a95
Rabina <32222576+rabinacathrine@users.noreply.github.com>
2020-03-23 18:57:46 +0530

    prograd

--- src/model/User.java
+++ src/model/User.java
@@ -1,3 +1,17 @@
 package model;
-
+import java.time.*;
 //Type your code here
+public class User{
+	String name;
+	String email;
+	LocalDate dob;
+	String gender;
+public User() {
+		name=name;
+		email=email;
+		dob=dob;
+		gender=gender;
+		//d=Integer.parseInt(date);
+		//m=Integer.parseInt(month);
+	}
+}
--- src/service/AstroCalculator.java
+++ src/service/AstroCalculator.java
@@ -1,3 +1,103 @@
 package service;
-
+import model.User;
 // Type your code
+public class AstroCalculator{
+	String result;
+	AstroCalculator asc=new AstroCalculator();
+	//model.User user=new model.User(name, email, dob, gender);
+	
+	public String findSign(model.User user,int day, String month) {
+		
+		String astro_sign=""; 
+        
+        // checks month and date within the  
+        // valid range of a specified zodiac 
+        if (month == "december"){ 
+              
+            if (day < 22) 
+            astro_sign = "Sagittarius"; 
+            else
+            astro_sign ="capricorn"; 
+        } 
+              
+        else if (month == "january"){ 
+            if (day < 20) 
+            astro_sign = "Capricorn"; 
+            else
+            astro_sign = "aquarius"; 
+        } 
+              
+        else if (month == "february"){ 
+            if (day < 19) 
+            astro_sign = "Aquarius"; 
+            else
+            astro_sign = "pisces"; 
+        } 
+              
+        else if(month == "march"){ 
+            if (day < 21)  
+            astro_sign = "Pisces"; 
+            else
+            astro_sign = "aries"; 
+        } 
+        else if (month == "april"){ 
+            if (day < 20) 
+            astro_sign = "Aries"; 
+            else
+            astro_sign = "taurus"; 
+        } 
+              
+        else if (month == "may"){ 
+            if (day < 21) 
+            astro_sign = "Taurus"; 
+            else
+            astro_sign = "gemini"; 
+        } 
+              
+        else if( month == "june"){ 
+            if (day < 21) 
+            astro_sign = "Gemini"; 
+            else
+            astro_sign = "cancer"; 
+        } 
+              
+        else if (month == "july"){ 
+            if (day < 23) 
+            astro_sign = "Cancer"; 
+            else
+            astro_sign = "leo"; 
+        } 
+              
+        else if( month == "august"){ 
+            if (day < 23)  
+            astro_sign = "Leo"; 
+            else
+            astro_sign = "virgo"; 
+        } 
+              
+        else if (month == "september"){ 
+            if (day < 23) 
+            astro_sign = "Virgo"; 
+            else
+            astro_sign = "libra"; 
+        } 
+              
+        else if (month == "october"){ 
+            if (day < 23) 
+            astro_sign = "Libra"; 
+            else
+            astro_sign = "scorpio"; 
+        } 
+              
+        else if (month == "november"){ 
+            if (day < 22) 
+            astro_sign = "scorpio"; 
+            else
+            astro_sign = "sagittarius"; 
+        } 
+              
+        System.out.println(astro_sign); 
+    
+		return result;
+	}
+}
